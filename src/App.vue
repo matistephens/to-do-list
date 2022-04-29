a02a7de9f11a0d5755819a82a47fe9efa8c04660
Matias Stephens <mstephens@riafinancial.com>
2022-04-28 18:21:15 -0600

    mi primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6 mx-auto text-center">
+      <h4 class="text-secondary mt-4">Todo List</h4>
+      <form class="mt-4" @submit.prevent="agregarNuevaTarea">
+        <div class="form-group row">
+          <div class="col-9">
+            <input v-model="nuevaTarea" type="text" class="form-control" placeholder="Ingresa una nueva tarea" required>
+          </div>
+          <div class="col-2">
+            <button type="submit" class=" btn btn-outline-secondary">Agregar</button>
+          </div>
+        </div>
+      </form>
+      <h4 class="text-secondary mt-4">Lista</h4>
+      <ul class="list-group">
+        <li v-for="(tarea, $index) in tareas" :key="$index" class="list-group-item">{{ $index + 1 }}. {{ tarea }}</li>
+      </ul>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: () => ({
+    nuevaTarea: "",
+    tareas: [
+      "Primera tarea",
+      "Segunda tarea"
+
+    ]
+  }),
+  methods: {
+    agregarNuevaTarea(){
+      this.tareas.unshift(this.nuevaTarea)
+      
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
